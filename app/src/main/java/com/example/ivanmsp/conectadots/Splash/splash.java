629ef8f558a62e1520d471bea54c607f8bcca48e
IvanMSP <ivanlopz11@gmail.com>
2016-04-01 22:36:29 -0600

    Funcionalidad Splashscreen con sonido

--- app/src/main/java/com/example/ivanmsp/conectadots/Splash/splash.java
+++ app/src/main/java/com/example/ivanmsp/conectadots/Splash/splash.java
@@ -3,8 +3,11 @@ package com.example.ivanmsp.conectadots.Splash;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.example.ivanmsp.conectadots.MainActivity;
 import com.example.ivanmsp.conectadots.R;
@@ -16,15 +19,19 @@ import butterknife.Bind;
  */
 public class splash extends Activity {
     protected boolean active=true;
-
+    private MediaPlayer reproductor;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splash);
+        reproductor=MediaPlayer.create(this,R.raw.sound);
+        reproductor.setLooping(true);
+        reproductor.start();
 
         Thread timerTread = new Thread(){
             public void run(){
@@ -41,9 +48,29 @@ public class splash extends Activity {
         };
         timerTread.start();
     }
+
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(reproductor.isPlaying())
+        {
+            reproductor.stop();
+            reproductor.release();
+        }
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        reproductor.start();
+    }
+
     @Override
     protected void onPause(){
         super.onPause();
+        reproductor.pause();
         finish();
     }
 }
